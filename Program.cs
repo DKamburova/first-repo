582e100ccb5b1e81a562df16f2de605825fb6f70
DKamburova <47268586+DKamburova@users.noreply.github.com>
2019-02-02 10:38:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a= int.Parse(Console.ReadLine());
+            int pregled = 0;
+            int nepregled = 0;
+            int sum1 = 0;
+            int sum2 = 0;
+            int lek = 7;
+            for (var i = 1;i<=a;i++)
+            {
+                var y = int.Parse(Console.ReadLine());
+                if (i % 3 == 0)
+                {
+                    if (sum1 > sum2) lek++;
+                }
+
+                if (y >=lek)
+                {
+                    nepregled = y - lek;
+                    pregled = lek;
+                }
+                else
+                {
+                    nepregled = 0;
+                    pregled = y;
+                }
+                sum1 += nepregled;
+                sum2 += pregled;
+            }
+            Console.WriteLine("Treated patients: {0}",sum2);
+            Console.WriteLine("Untreated patients: {0}", sum1);
+        }
+    }
+}
